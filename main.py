f293703cfdb0fa0a110b1b278499feca1af9afa4
renwish <eugenebasinio@gmail.com>
2022-11-12 12:23:27 +0800

    input validation system complete! and test for option 1

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 #display menu
+print("Menu:\n 1.) Add an item\n 2.) Search\n 3.) Exit (y/n)\n")
+
 #select item in menu
+option = input("What do you want to do? (1-3): ")
+
 #perform option
-    #1. ask data for contact tracing
-    #2. search full name, then display
-    #3. exit or retry
+#1. ask data for contact tracing
+while option not in ['1','2','3']:
+    print('ERROR! select a number from 1-3')
+    option = input("What do you want to do? (1-3): ")
+
+if option == '1':
+    print('yay')
+
+
+#2. search full name, then display
+#3. exit or retry
